e0d4f5e8dcffa292712b86c8e72692754cd170f8
marcelo-sl <mar.sousa2061@gmail.com>
2024-08-26 20:08:45 -0300

    Create project

---
+++ Habito/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Habito
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
